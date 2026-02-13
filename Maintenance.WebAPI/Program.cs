8e957c02b610c3120a119d66bdb0a40d339fbc4d
Robbie <ralazer3864@conestogac.on.ca>
2026-02-13 15:23:32 -0500

    Fixed swagger enable swagger in production

--- Maintenance.WebAPI/Program.cs
+++ Maintenance.WebAPI/Program.cs
@@ -9,11 +9,9 @@ builder.Services.AddScoped<IRepairHistoryService, FakeRepairHistoryService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.MapControllers();
