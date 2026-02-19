f84a4a04681ed9a503547671e38ce909abf2e5b4
Robbie <ralazer3864@conestogac.on.ca>
2026-02-19 15:21:38 -0500

    update MVC controller and nav layout

--- 8768364RobbieAlazer_MVC/Controllers/MaintenanceController.cs
+++ 8768364RobbieAlazer_MVC/Controllers/MaintenanceController.cs
@@ -12,13 +12,27 @@ namespace _8768364RobbieAlazer_MVC.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
+
         public async Task<IActionResult> Usage()
         {
             var client = _httpClientFactory.CreateClient("MaintenanceApi");
-            var result = await client.GetFromJsonAsync<object>("api/RepairHistory/usage");
+            var result = await client.GetFromJsonAsync<object>("api/maintenance/usage");
             return View(result);
         }
 
+        public async Task<IActionResult> Transfer(int fromId, int toId, decimal amount)
+        {
+            var client = _httpClientFactory.CreateClient("MaintenanceApi");
+
+            var response = await client.PostAsync(
+                $"api/RepairHistory/transfer?fromId={fromId}&toId={toId}&amount={amount}",
+                null);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            ViewBag.Result = content;
+            return View();
+        }
 
         [HttpGet]
         public IActionResult History()
